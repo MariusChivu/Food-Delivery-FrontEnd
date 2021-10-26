62b0b620dabcb793792b2c118cf7328dbf68f158
MariusChivu <marius.chivu95@yahoo.com>
2021-10-26 12:33:53 +0300

    Update cont.php

--- cont.php
+++ cont.php
@@ -4,9 +4,39 @@ include("continut/meniu.php"); ?>
 
 <div class='container pagina-cont mt-5 mb-5'>
 	<div class='row d-flex justify-content-center'>
-		<div class='cld-md-12 border border-dark'><h1>Panou utilizator</h1></div>
-		<div class='col-md-4 border m-3 p-3 bg-primary'></div>
-		<div class='col-md-4 border m-3 p-3 bg-primary'></div>
+		<div class='cld-md-12 m-4'><h1>Panou utilizator</h1></div>
+		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-user-edit"></i> Informatii cont</h2>
+			<p>Gestioneaza informatii despre cont.</p>
+		</div>
+		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-user-cog"></i> Setari cont</h2>
+			<p>Gestioneaza setarile despre contului.</p>
+		</div>
+		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-receipt"></i> Comenzile mele</h2>
+			<p>Istoricul comenzilor.</p>
+		</div>
+		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-map-marked"></i> Adresele mele</h2>
+			<p>Salveaza o adresa pentru livrare.</p>
+		</div>
+		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-credit-card"></i> Metode de plata</h2>
+			<p>Modifica metodele tale de plata.</p>
+		</div>
+		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-ticket-alt"></i> Vouchere</h2>
+			<p>Adauga un voucher de reducere.</p>
+		</div>
+		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-gift"></i> Invita</h2>
+			<p>Invita-ti prietenii si primesti un voucher de reducere cadou.</p>
+		</div>
+		<a href="index.php" class='col-md-4 border  border-dark m-3 p-3 pointer'>
+			<h2><i class="fas fa-sign-out-alt"></i> Deconectare</h2>
+			<p>Deconectare de la cont.</p>
+		</a>
 			
 	</div>
 </div>
