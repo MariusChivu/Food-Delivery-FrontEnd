a5868eae2cd94f26431c54a02b2e9686c140dd26
MariusChivu <marius.chivu95@yahoo.com>
2021-10-26 11:34:18 +0300

    Update cont.php

--- cont.php
+++ cont.php
@@ -2,9 +2,11 @@
 include("continut/meniu.php"); ?>
 
 
-<div class='container pagina-cont'>
-	<div class='row'>
-
+<div class='container pagina-cont mt-5 mb-5'>
+	<div class='row d-flex justify-content-center'>
+		<div class='cld-md-12 border border-dark'><h1>Panou utilizator</h1></div>
+		<div class='col-md-4 border m-3 p-3 bg-primary'></div>
+		<div class='col-md-4 border m-3 p-3 bg-primary'></div>
 			
 	</div>
 </div>
