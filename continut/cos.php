e3130e2677c33b4a053d083a33585d05f31357fd
MariusChivu <marius.chivu95@yahoo.com>
2021-10-26 14:59:48 +0300

    Update cos.php

--- continut/cos.php
+++ continut/cos.php
@@ -1,28 +1,81 @@
 <div class="modal fade" id="cos">
-	<div class="modal-dialog">
+	<div class="modal-dialog modal-dialog-scrollable modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
 				<h4 class="modal-title"><i class="fas fa-shopping-cart"></i> Cosul tau</h4>
 				<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 			</div>
-			<form class='form' action='' method='POST'>
-				<div class="modal-body">
-					<strong class='w-100 d-flex justify-content-center fs-1'><i class="fas fa-user-tie"></i></strong>
-					<div class="input-group mt-4">
-						<span class="input-group-text">E-mail</span>
-						<input type="text" class="form-control" placeholder="E-mail" autocomplete="off" reuqired>
-					</div>
-					<div class="input-group mt-4 mb-4">
-						<span class="input-group-text">Parola</span>
-						<input type="password" class="form-control" placeholder="Parola" autocomplete="off" reuqired>
-					</div>
-				
-				</div>
-				<div class="modal-footer">
-					<a href="cont.php" class="btn btn-success">Conectare</a>
-					<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Închide</button>
-				</div>
-			</form>
+			<div class="modal-body">
+				<h2>McDonald`s</h2>
+
+				<table class='table table-striped table-hover table-cos'>
+					<th>#</th>
+					<th>Nume</th>
+					<th>Cantitate</th>
+					<th>Pret</th>
+					<th>Total</th>
+					<th><i class="fas fa-trash"></i></th>
+
+					<tr>
+						<td><img src='assets/img/restaurante/mcdonalds/prod0.webp'></td>
+						<td>HOME MENU</td>
+						<td>1</td>
+						<td>60 RON</td>
+						<td>60 RON</td>
+						<td><i class="fas fa-trash pointer"></i></td>
+					</tr>
+					<tr>
+						<td><img src='assets/img/restaurante/mcdonalds/prod3.webp'></td>
+						<td>MENIU MAXI BIG MAC</td>
+						<td>2</td>
+						<td>21.20 RON</td>
+						<td>42.40 RON</td>
+						<td><i class="fas fa-trash pointer"></i></td>
+					</tr>
+					<tr>
+						<td><img src='assets/img/restaurante/mcdonalds/prod5.webp'></td>
+						<td>MENIU MAXI CHICKEN GRILL</td>
+						<td>1</td>
+						<td>24.40 RON</td>
+						<td>24.4 RON</td>
+						<td><i class="fas fa-trash pointer"></i></td>
+					</tr>
+					<tr>
+						<td><img src='assets/img/restaurante/mcdonalds/prod25.webp'></td>
+						<td>CARTOFI PRAJIȚI TWISTER</td>
+						<td>1</td>
+						<td>7.8 RON</td>
+						<td>7.8 RON</td>
+						<td><i class="fas fa-trash pointer"></i></td>
+					</tr>
+					<tr>
+						<td><img src='assets/img/restaurante/mcdonalds/prod26.webp'></td>
+						<td>CARTOFI PRAJIȚI PORȚIE MARE</td>
+						<td>3</td>
+						<td>7.80 RON</td>
+						<td>23.40 RON</td>
+						<td><i class="fas fa-trash pointer"></i></td>
+					</tr>
+					<tr>
+						<td><img src='assets/img/restaurante/mcdonalds/prod34.webp'></td>
+						<td>COCA-COLA</td>
+						<td>4</td>
+						<td>6.60 RON</td>
+						<td>26.40 RON</td>
+						<td><i class="fas fa-trash pointer"></i></td>
+					</tr>
+
+				</table>
+
+
+			
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-success" data-bs-dismiss="modal">Finalizare comanda</button>
+				<button type="button" class="btn btn-warning" data-bs-dismiss="modal">Resetare cos</button>
+				<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Închide</button>
+			</div>
 		</div>
 	</div>
 </div>
+
