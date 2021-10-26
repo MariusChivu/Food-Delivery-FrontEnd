1ef55ebcbef3b419dd947a08d75010bb4f73b6ce
MariusChivu <marius.chivu95@yahoo.com>
2021-10-26 11:21:32 +0300

    Update cont.php

--- cont.php
+++ cont.php
@@ -2,21 +2,9 @@
 include("continut/meniu.php"); ?>
 
 
-<div class='container pagina-oras'>
+<div class='container pagina-cont'>
 	<div class='row'>
-		<div class='col-lg-10'>
-			<div class='bara-header'>
-				<h1>Livrare de mâncare de la cele mai bune restaurante din orașul <strong id='numeOras'></strong></h1>
-			</div>
-		</div>
-	</div>
-</div>
 
-<div class='container oras-restaurant'>
-	<div class='row'>
-			<div class='oras-container-restaurant' id='container-oras-restaurant'>
-				<!-- Continut completat din javascript cu for -->
-			</div>
 			
 	</div>
 </div>
