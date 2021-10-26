9fbd4772a043c4d4523a36d57c409b10345d90b0
MariusChivu <marius.chivu95@yahoo.com>
2021-10-26 17:19:24 +0300

    Update cont.php

--- cont.php
+++ cont.php
@@ -6,12 +6,12 @@ include("continut/meniu.php"); ?>
 	<div class='row d-flex justify-content-center'>
 		<div class='cld-md-12 m-4'><h1>Panou utilizator</h1></div>
 		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
-			<h2><i class="fas fa-user-edit"></i> Informatii cont</h2>
-			<p>Gestioneaza informatii despre cont.</p>
+			<h2><i class="fas fa-user-edit"></i> Informații cont</h2>
+			<p>Gestionează informaâiile despre cont.</p>
 		</div>
 		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
 			<h2><i class="fas fa-user-cog"></i> Setari cont</h2>
-			<p>Gestioneaza setarile despre contului.</p>
+			<p>Gestionează setările despre contului.</p>
 		</div>
 		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
 			<h2><i class="fas fa-receipt"></i> Comenzile mele</h2>
@@ -19,19 +19,19 @@ include("continut/meniu.php"); ?>
 		</div>
 		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
 			<h2><i class="fas fa-map-marked"></i> Adresele mele</h2>
-			<p>Salveaza o adresa pentru livrare.</p>
+			<p>Salvează o adresă pentru livrare.</p>
 		</div>
 		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
-			<h2><i class="fas fa-credit-card"></i> Metode de plata</h2>
-			<p>Modifica metodele tale de plata.</p>
+			<h2><i class="fas fa-credit-card"></i> Metode de plată</h2>
+			<p>Modifică metodele tale de plată.</p>
 		</div>
 		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
 			<h2><i class="fas fa-ticket-alt"></i> Vouchere</h2>
-			<p>Adauga un voucher de reducere.</p>
+			<p>Adaugă un voucher de reducere.</p>
 		</div>
 		<div class='col-md-4 border  border-dark m-3 p-3 pointer'>
-			<h2><i class="fas fa-gift"></i> Invita</h2>
-			<p>Invita-ti prietenii si primesti un voucher de reducere cadou.</p>
+			<h2><i class="fas fa-gift"></i> Invită</h2>
+			<p>Invită-ți prietenii și primești un voucher de reducere cadou.</p>
 		</div>
 		<a href="index.php" class='col-md-4 border  border-dark m-3 p-3 pointer'>
 			<h2><i class="fas fa-sign-out-alt"></i> Deconectare</h2>
