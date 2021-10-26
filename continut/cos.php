c27a77abe7b63ff7851adc0b39854317ca9052a3
MariusChivu <marius.chivu95@yahoo.com>
2021-10-26 14:23:23 +0300

    Create cos.php

---
+++ continut/cos.php
@@ -0,0 +1,28 @@
+<div class="modal fade" id="cos">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title"><i class="fas fa-sign-in-alt"></i> Conectare</h4>
+				<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+			</div>
+			<form class='form' action='' method='POST'>
+				<div class="modal-body">
+					<strong class='w-100 d-flex justify-content-center fs-1'><i class="fas fa-user-tie"></i></strong>
+					<div class="input-group mt-4">
+						<span class="input-group-text">E-mail</span>
+						<input type="text" class="form-control" placeholder="E-mail" autocomplete="off" reuqired>
+					</div>
+					<div class="input-group mt-4 mb-4">
+						<span class="input-group-text">Parola</span>
+						<input type="password" class="form-control" placeholder="Parola" autocomplete="off" reuqired>
+					</div>
+				
+				</div>
+				<div class="modal-footer">
+					<a href="cont.php" class="btn btn-success">Conectare</a>
+					<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Închide</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
