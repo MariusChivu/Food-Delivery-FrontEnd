1d9ede009b716044c8454ad2a9fd4067ebce3e6e
MariusChivu <marius.chivu95@yahoo.com>
2021-10-27 13:16:57 +0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,29 @@
+<?php include("continut/header.php");
+include("continut/meniu.php"); ?>
+
+<div class='container mt-5 mb-5'>
+	<div class='row justify-content-center align-items-center'>
+		<div class='col-md-12'><h1>Contact</h1></div>
+		<div class='col-md-4'>
+			<h3><i class="fas fa-map-marker-alt"></i> Adresa</h3>
+			<h4>București, Str Centrală nr 1</h4>
+		</div>
+		<div class='col-md-8'>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d91160.56672518827!2d26.024597973154542!3d44.43792692053395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1f93abf3cad4f%3A0xac0632e37c9ca628!2sBucharest!5e0!3m2!1sen!2sro!4v1635328121313!5m2!1sen!2sro" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+		</div>
+		<div class='col-md-6 mt-5 mb-5'>
+			<h3><i class="fas fa-phone-alt"></i> Telefon</h3>
+			<h4>0123456789</h4>
+			<h4>0123456789</h4>
+		</div>
+		<div class='col-md-6 mt-5 mb-5'>
+			<h3><i class="fas fa-envelope"></i> E-mail</h3>
+			<h4>contact@mail.com</h4>
+			<h4>contact@mail.com</h4>
+		</div>
+	</div>
+</div>
+
+
+<?php include("continut/footer.php"); ?>
+<script> popRest(); </script>
